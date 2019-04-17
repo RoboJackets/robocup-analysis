07c4c58e99590aa2f0e97098a138ef8657fca77f
Kyle Stachowicz <kylestach99@gmail.com>
2019-04-16 20:53:45 -0400

    Use real-ish robot constants

--- model-python/sim.py
+++ model-python/sim.py
@@ -8,8 +8,8 @@ import vis
 def main():
     clock = pygame.time.Clock()
 
-    pos = np.asmatrix([0, 1, 0.]).T
-    vel = np.asmatrix([1, 0, 1.]).T
+    pos = np.asmatrix([0, 0, 0.]).T
+    vel = np.asmatrix([0, 0, 0.]).T
     visualizer = vis.Visualizer()
 
     fps = 60
@@ -21,16 +21,15 @@ def main():
 
     while not visualizer.close:
         visualizer.update_events()
-        v = 7
+        v = 1.5
         rx = np.asmatrix([np.sin(v * t), np.cos(v * t), v * t]).T
-        rv = v * np.asmatrix([np.cos(v * t), -np.sin(v * t), v]).T
+        rv = v * np.asmatrix([np.cos(v * t), -np.sin(v * t), 1]).T
         ra = v ** 2 * np.asmatrix([-np.sin(v * t), -np.cos(v * t), 0]).T
 
-        visualizer.draw(pos, rx)
-
-        u = 0.8 * controller.control(pos, vel, rx, rv, ra)
+        u = controller.control(pos, vel, rx, rv, ra)
 
         vdot = model.forward_dynamics(vel, u, pos[2, 0])
+        visualizer.draw(pos, rx)
 
         pos += dt * vel + 0.5 * vdot * dt ** 2
         vel += dt * vdot
@@ -38,12 +37,12 @@ def main():
         clock.tick(60)
         t += dt
         i += 1
-        # if t > 20:
-        #     t = 0.0
-        #     i = 0
-        #     pos = np.asmatrix([0, 1, 3.]).T
-        #     vel = np.asmatrix([1, 0, 1.]).T
-        #     controller.reset()
+        if t > 20:
+            t = 0.0
+            i = 0
+            pos = np.asmatrix([0, 1, 3.]).T
+            vel = np.asmatrix([1, 0, 1.]).T
+            controller.reset()
 
 
 if __name__ == '__main__':
